697fbff955c90cd44d941cfb3081505a32b7edc8
septiawanajipradana <septiawanajipradana@gmail.com>
2023-11-16 22:28:16 +0700

    fix: delete useless file

--- controller/BotController.js
+++ controller/BotController.js
@@ -4,7 +4,6 @@ const f = require("../utils/Formatter");
 module.exports = class BotController extends Controller {
 
     async introduction(request) {
-      await this.deleteState();
       return this.reply("Halo")
     }
 
