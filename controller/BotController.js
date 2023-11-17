776b77ea52c2ab35ae00e6dbb0ca027c12224a51
septiawanajipradana <septiawanajipradana@gmail.com>
2023-11-17 13:08:17 +0700

    fix: delete allowed_numbers

--- controller/BotController.js
+++ controller/BotController.js
@@ -3,8 +3,24 @@ const f = require("../utils/Formatter");
 
 module.exports = class BotController extends Controller {
 
+
     async introduction(request) {
-      return this.reply("Halo")
+      return Response.menu.fromArrayOfString(
+        [
+          f("menu.daftarProduk"),
+          f("menu.alamatKantor")
+        ],
+        f("intro", [request.name]),
+        f("template.menu")
+      );
+    }
+
+    async product(request) {
+      return this.reply("Ini produk digital saya, bisa dikunjungi di http://dewakoding.com")
+    }
+
+    async alamatKantor(request) {
+      return this.reply("Alamat kantor kami ada di Jakarta")
     }
 
 }
--- router.js
+++ router.js
@@ -4,6 +4,8 @@ const f = require("./utils/Formatter");
 
 const router = new Router();
 
+router.menu(f("menu.daftarProduk"), [BotController, "product"]);
+router.menu(f("menu.alamatKantor"), [BotController, "alamatKantor"]);
 router.keyword("*", [BotController, "introduction"]);
 
 module.exports = router;
